3f486dda7b33f83880f5db314d565e436e6443a4
younha00 <ayeon0@office.skhu.ac.kr>
2024-03-15 15:31:57 +0900

    ✨ [#3]Feat: Auth, Common 디렉토리 생성
    인증과 관련된 Auth, 공통적으로 쓰이는 Common 디렉토리 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
