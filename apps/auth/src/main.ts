03929dd36ebe904ea907ed63fdb7fb2df8596871
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-13 18:49:50 +0900

    🚀 [#15]Feat: auth 프로젝트 도커라이징
    아직 jwt랑 암호화 로직 하지 않았음

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.useLogger(app.get(Logger));
+  await app.listen(3001);
 }
 bootstrap();
