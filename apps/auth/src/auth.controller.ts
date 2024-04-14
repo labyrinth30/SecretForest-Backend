e4fc8f3fc293d52e129d4656a9c84895b93edb4d
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-14 22:44:00 +0900

    [#15]Chore: current-user.decorator를 common 폴더로 옮김

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUser } from '@app/common';
 import { UserDocument } from './users/models/user.schema';
 import { Response } from 'express';
 import { MessagePattern, Payload } from '@nestjs/microservices';
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,5 @@ export * from './database';
 export * from './logger';
 export * from './auth';
 export * from './const';
+export * from './decorators';
+export * from './dto';
