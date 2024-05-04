59bf44857722b57c482d3620f131f4c92a9a5f30
younha00 <ayeon0@office.skhu.ac.kr>
2024-05-04 14:45:55 +0900

    [#44]Refactor: 테마 api에 Roles 데코레이터 추가

--- apps/reservations/src/themes/themes.controller.ts
+++ apps/reservations/src/themes/themes.controller.ts
@@ -7,31 +7,41 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { ThemesService } from './themes.service';
 import { CreateThemeDto } from './dto/create-theme.dto';
 import { UpdateThemeDto } from './dto/update-theme.dto';
+import { JwtAuthGuard, Roles } from '@app/common';
 
 @Controller('themes')
 export class ThemesController {
   constructor(private readonly themesService: ThemesService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @Roles('Admin')
   create(@Body() createThemeDto: CreateThemeDto) {
     return this.themesService.create(createThemeDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @Roles('Admin')
   findAll() {
     return this.themesService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles('Admin')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.themesService.findThemeById(id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles('Admin')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateThemeDto: UpdateThemeDto,
@@ -40,6 +50,8 @@ export class ThemesController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles('Admin')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.themesService.remove(id);
   }
